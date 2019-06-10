d907047acd7f489d91cc3545acb1e8e809905c11
Bastien <bast.schaeffer@gmail.com>
2019-06-10 12:07:25 +0100

    fix: fix

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-console.log('bastien-versioning with updated version beta test and deploy with tag and commit');
+console.log('bastien-versioning with updated version beta test and deploy with tag and commit on master');
